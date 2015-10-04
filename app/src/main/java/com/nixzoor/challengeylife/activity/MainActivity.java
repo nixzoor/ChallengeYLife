be7b0434e4977f4b87f4fa22a3f5924711a98b5e
nixzoor <nik.orter@gmail.com>
2015-10-04 22:27:14 +0200

    project structure

--- app/src/main/java/com/nixzoor/challengeylife/activity/MainActivity.java
+++ app/src/main/java/com/nixzoor/challengeylife/activity/MainActivity.java
@@ -1,9 +1,11 @@
-package com.nixzoor.challengeylife;
+package com.nixzoor.challengeylife.activity;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.nixzoor.challengeylife.R;
 
 
 public class MainActivity extends ActionBarActivity {
